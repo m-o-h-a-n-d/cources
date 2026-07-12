f9c849d00f527f71f744a0d5913e4048f089704f
Mohaned Ahmed <mohandahmed015529.222.00@gmail.com>
2026-07-12 17:06:18 +0300

    Second Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mohanad/student-course-management',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e2a91b6698a30ff6b1445c10aa8fd983e1f2af77',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mohanad/student-course-management' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e2a91b6698a30ff6b1445c10aa8fd983e1f2af77',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
