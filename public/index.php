cd665cf563e95fe321124d42f5acf40a2e38f9be
Mohaned Ahmed <mohandahmed015529.222.00@gmail.com>
2026-07-12 22:34:11 +0300

    Db Config

--- public/index.php
+++ public/index.php
@@ -5,7 +5,15 @@ use Core\Router;
 use Core\Container;
 
 require '../vendor/autoload.php';
+var_dump(
+    file_exists(__DIR__.'/../app/Http/Controllers/Admin/StudentController.php')
+);
 
+var_dump(
+    class_exists(\App\Http\Controllers\Admin\StudentController::class)
+);
+
+die();
 if (session_status() === PHP_SESSION_NONE) {
 	session_start();
 }
