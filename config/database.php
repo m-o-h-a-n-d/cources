926a73141c60f2148d3d302a1ba7f7c72c2ca564
Mohaned Ahmed <mohandahmed015529.222.00@gmail.com>
2026-07-12 22:08:22 +0300

    Second Commit

--- config/database.php
+++ config/database.php
@@ -1,14 +1,10 @@
-<?php 
-
-
+<?php
 
 return [
-    'host' => 'localhost',
-    'dbname' => 'courses',
-    'username' => 'root',
-    'password' => 'mohandpoh09867',
+    'host' => 'db.fr-pari1.bengt.wasmernet.com',
+    'port' => '10272',
+    'dbname' => 'coursesdb',
+    'username' => 'user_31cfaffb',
+    'password' => 'pw_W204bxgx0kR45pIC0qhLUqn3HLojPuKc',
     'charset' => 'utf8mb4',
 ];
-
-
-
